a6e6905f9f4d64100361cda166bcb08a1ddef51e
code-loading <eahmed711@gmail.com>
2020-06-17 12:32:13 +0400

    insignificant changes

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -11,7 +11,8 @@ export const Transaction = ({transaction}) => {
     return (
         
         <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
+            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+            <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
         </li>
                 
     );
--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -27,17 +27,21 @@ export const TransactionForm = () => {
     return(<>
         <h3>Add new transactions</h3>
         <form onSubmit={onSubmit}>
+            
             <div className="form-control">
                 <label htmlFor="text">Transaction purpose</label>
                 
                 <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." /> 
             </div>
+            
             <div className="form-control">
                 <label htmlFor="amount">Amount <br/>
                 (negative values are expenses, positive values are income)</label>
                 <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
             </div>
+            
             <button className="btn">Add transaction</button>
+        
         </form>
     </>);
 }
